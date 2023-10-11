bb707c8c7e67e237559f3241628f45f7a5c1c01e
guenoel <guenooel@hotmail.com>
2023-10-11 08:41:49 -0700

    task 1

---
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -0,0 +1,27 @@
+
+#include "rb_trees.h"
+
+int recursive_check(const rb_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->color == RED && tree->parent->color == RED)
+		return (0);
+	if (tree->color == RED && (!tree->left != !tree->right))
+		return (0);
+	return (recursive_check(tree->left) && recursive_check(tree->right));
+}
+
+/**
+ * rb_tree_is_valid - check if red black tree is valid
+ * @tree: root of red-black tree
+ * Return: 1 if true 0 if false
+*/
+int rb_tree_is_valid(const rb_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->color != BLACK)
+		return (0);
+	return (recursive_check(tree));
+}
