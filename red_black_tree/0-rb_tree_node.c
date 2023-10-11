17ff48b2fdc70bd1742391ccf0046f3eba47878c
guenoel <guenooel@hotmail.com>
2023-10-11 02:30:29 -0700

    bettyfy code

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -1,12 +1,19 @@
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node - create new node for the tree
+ * @parent: parent node
+ * @value: integer in node
+ * @color: color of the node (red or black)
+ * Return: new node
+*/
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *new_node;
+
 	new_node = (rb_tree_t *)malloc(sizeof(rb_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = value;
 	new_node->color = color;
 	new_node->parent = parent;
