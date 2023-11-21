56ba34204ac8412580a4079ea0b21a12cd950cf7
guenoel <guenooel@hotmail.com>
2023-11-21 07:54:16 -0800

    make two cases logic

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -2,6 +2,27 @@
 #include <string.h>
 
 /**
+ * create_vertex - add vertex in a graph
+ * @str: string to copy in vertex
+ * @nb_vertices: number of vertices
+ * Return: Pointer to the new vertex
+ */
+vertex_t *create_vertex(const char *str, size_t nb_vertices)
+{
+	vertex_t *new_vertex = (vertex_t *)malloc(sizeof(vertex_t));
+
+	if (!new_vertex)
+		return (NULL);
+	new_vertex->content = strdup(str);
+
+	new_vertex->edges = NULL;
+	new_vertex->index = nb_vertices;
+	new_vertex->nb_edges = 0;
+	new_vertex->next = NULL;
+	return (new_vertex);
+}
+
+/**
  * graph_add_vertex - add vertex in a graph
  * @graph: pointer to the graph to add to
  * @str: string to copy in vertex
@@ -9,27 +30,32 @@
  */
 vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
-	vertex_t *current_vertex = graph->vertices;
-	vertex_t *new_vertex = (vertex_t *)malloc(sizeof(vertex_t));
+	vertex_t *current_vertex, *new_vertex;
 
-	if (!new_vertex)
+	/* Check for NULL parameters */
+	if (graph == NULL || str == NULL)
 		return (NULL);
 
-	strcpy(new_vertex->content, str);
-	new_vertex->edges = NULL;
-	new_vertex->index = 0;
-	new_vertex->nb_edges = 0;
-	new_vertex->next = NULL;
-
-	graph->nb_vertices++;
-	if (!current_vertex)
-		graph->vertices = new_vertex;
-	while (!current_vertex->next)
+	/* Case of empty graph */
+	if (!graph->vertices)
 	{
-		new_vertex->index++;
+		new_vertex = create_vertex(str, graph->nb_vertices);
+		graph->vertices = new_vertex;
+		graph->nb_vertices++;
+		return (new_vertex);
+	}
+
+	/* Case of non-empty graph */
+	current_vertex = graph->vertices;
+	while (current_vertex->next != NULL)
+	{
+		/* check if vertice content already exist */
+		if (!strcmp(current_vertex->content, str))
+			return (NULL);
 		current_vertex = current_vertex->next;
 	}
+	new_vertex = create_vertex(str, graph->nb_vertices);
 	current_vertex->next = new_vertex;
-
+	graph->nb_vertices++;
 	return (new_vertex);
 }
