b430f8ee67aeee28d1243271fdd18479c0589b1d
guenoel <guenooel@hotmail.com>
2023-12-24 11:13:20 -0800

    create symbol node - task 5

---
+++ huffman_coding/symbol.c
@@ -0,0 +1,22 @@
+#include "huffman.h"
+
+/**
+ * symbol_create - Creates a symbol_t data structure
+ *
+ * @data: The character
+ * @freq: The associated frequency
+ *
+ * Return: Pointer to the created symbol_t, or NULL on failure
+ */
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *symbol = malloc(sizeof(symbol_t));
+
+	if (symbol == NULL)
+		return (NULL);
+
+	symbol->data = data;
+	symbol->freq = freq;
+
+	return (symbol);
+}
